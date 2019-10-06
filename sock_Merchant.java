10b4c958c8a2c5d8dc28c11e85ebf45cb2277541
somya-jain28 <50552755+somya-jain28@users.noreply.github.com>
2019-10-06 09:34:19 +0530

    Add files via upload
    hackerRank warmup ,ques1: Sock Merchant

---
+++ sock_Merchant.java
@@ -0,0 +1,16 @@
+static int sockMerchant(int n, int[] ar) {
+    HashSet<Integer> set=new HashSet<Integer>();
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        int element=ar[i];
+        if(set.contains(element))
+        {
+        set.remove(element);
+        count++;
+        }
+        else
+        set.add(element);
+    }
+    return count;
+}
